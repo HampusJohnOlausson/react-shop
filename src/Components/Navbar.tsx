2e3dd91fb3257e5ce6d388f5ff5f43d2625ce04b
Hampus Olausson <hampus.john.olausson@gmail.com>
2021-03-29 16:50:53 +0200

    navbar styling done

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -2,26 +2,33 @@ import { makeStyles } from '@material-ui/core';
 import React from 'react'
 
 const useStyles = makeStyles((theme) => ({
-
   root: {
-    minHeight: '10vh',
-    background: 'red',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
+    minHeight: "10vh",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    textShadow: "2px 2px 5px rgba(0, 0, 0, 0.3)",
   },
   nav: {
-    width: '50%',
+    width: "50%",
   },
   list: {
-    width: '100%',
-    display: 'flex',
-    justifyContent: 'space-between',
-    listStyle: 'none',
-    fontSize: '1.2rem',
+    width: "100%",
+    display: "flex",
+    justifyContent: "space-between",
+    listStyle: "none",
+    fontSize: "1.2rem",
   },
-
+  home: {
+    fontFamily: "Yellowtail, cursive",
+    color: "#FFBD2D",
+    fontWeight: "bold",
+    fontSize: "2.5rem",
+  },
+  icon: {
+    fontSize: "2.5rem",
+  },
 }));
 
 const Navbar = () => {
@@ -30,14 +37,18 @@ const Navbar = () => {
 
     return (
       <header className={classes.root}>
-        <nav style={{width: '50%'}}>
+        <nav style={{ width: "50%" }}>
           <ul className={classes.list}>
-            <li>R</li>
-            <li>Meny</li>
-            <li>Boka Bord</li>
-            <li>Kontakt</li>
-            <li>Twitter</li>
-            <li>Instagram</li>
+            <li className={classes.home}>R</li>
+            <li style={{ marginTop: "1rem" }}>Meny</li>
+            <li style={{ marginTop: "1rem" }}>Boka Bord</li>
+            <li style={{ marginTop: "1rem" }}>Kontakt</li>
+            <li className={classes.icon}>
+              <i className="fab fa-twitter"></i>
+            </li>
+            <li className={classes.icon}>
+              <i className="fab fa-instagram"></i>
+            </li>
           </ul>
         </nav>
       </header>
