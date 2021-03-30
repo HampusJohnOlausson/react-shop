65d95aa2c9399e7a2b0b8e2dd133d4fc540cba80
Hampus Olausson <hampus.john.olausson@gmail.com>
2021-03-30 15:24:02 +0200

    added information for each product

---
+++ src/ProductData.tsx
@@ -0,0 +1,39 @@
+export interface Product {
+
+    image: string,
+    title: string,
+    price: number,
+    info: string,
+    quantity: number,
+}
+
+export const ProductData: Product[] = [
+  {
+    image: "hhh",
+    title: "Pizza Margarita",
+    price: 139,
+    info: "gjord på bla bla",
+    quantity: 1,
+  },
+  {
+    image: "hhh",
+    title: "Pizza Margarita",
+    price: 139,
+    info: "gjord på bla bla",
+    quantity: 1,
+  },
+  {
+    image: "hhh",
+    title: "Pizza Margarita",
+    price: 139,
+    info: "gjord på bla bla",
+    quantity: 1,
+  },
+  {
+    image: "hhh",
+    title: "Pizza Margarita",
+    price: 139,
+    info: "gjord på bla bla",
+    quantity: 1,
+  },
+];
