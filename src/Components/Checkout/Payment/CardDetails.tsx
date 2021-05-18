1d83b23725d414728336f91c95366e93e52d465c
Hampus Olausson <hampus.john.olausson@gmail.com>
2021-05-18 17:05:53 +0200

    card validation done

--- src/Components/Checkout/Payment/CardDetails.tsx
+++ src/Components/Checkout/Payment/CardDetails.tsx
@@ -17,8 +17,41 @@ const CardDetails = () => {
         setErrorName("");
       }
       console.log(e.target.value);
-      // payment.addName(e);
+      // payment.addCardName(e);
     };
+
+    const handleNumberError = (e: ChangeEvent<HTMLInputElement>) => {
+      if (!/^(?:[0-9]{16}|[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4})$/.test(e.target.value)
+      ) {
+        setErrorNumber("Cardnumber is not valid");
+      } else {
+        setErrorNumber("");
+      }
+      console.log(e.target.value);
+      // payment.addCardNumber(e);
+    };
+
+    const handleExpirationError = (e: ChangeEvent<HTMLInputElement>) => {
+      if (!/^(?:[0-9]{4}|[0-9]{2}-[0-9]{2})$/.test(e.target.value)) {
+        setErrorExpiration("Expiration date is not valid");
+      } else {
+        setErrorExpiration("");
+      }
+      console.log(e.target.value);
+      // payment.addExpiration(e);
+    };
+
+    const handleCvcError = (e: ChangeEvent<HTMLInputElement>) => {
+      if (!/^(?:[0-9]{3}|[0-9]{3})$/.test(e.target.value)) {
+        setErrorCvc("CVC is not valid");
+      } else {
+        setErrorCvc("");
+      }
+      console.log(e.target.value);
+      // payment.addCvc(e);
+    };
+
+
 
     return (
       <div>
@@ -36,28 +69,56 @@ const CardDetails = () => {
               label="Creditcard Name"
               margin="normal"
               fullWidth
-              id="lastName"
-              key="lastName"
-              autoComplete="lastName"
+              placeholder="Fullname"
+              id="cardName"
+              key="cardName"
+              autoComplete="cardName"
               autoFocus
             />
             <TextField
+              onChange={handleNumberError}
+              helperText={errorNumber}
+              error={Boolean(errorNumber)}
               variant="outlined"
               required
               label="Creditcard Number"
               margin="normal"
+              fullWidth
+              placeholder="0000 0000 0000 0000"
+              id="cardNumber"
+              key="cardNumber"
+              autoComplete="cardNumber"
+              autoFocus
             />
             <TextField
+              onChange={handleExpirationError}
+              helperText={errorExpiration}
+              error={Boolean(errorExpiration)}
               variant="outlined"
               required
               label="Expiration Date"
               margin="normal"
+              fullWidth
+              placeholder="00/00"
+              id="expires"
+              key="expires"
+              autoComplete="expires"
+              autoFocus
             />
             <TextField
+              onChange={handleCvcError}
+              helperText={errorCvc}
+              error={Boolean(errorCvc)}
               variant="outlined"
               required
               label="CVC"
               margin="normal"
+              fullWidth
+              placeholder="CVC"
+              id="cvc"
+              key="cvc"
+              autoComplete="cvc"
+              autoFocus
             />
           </AccordionDetails>
           <Button className={classes.btn}>Complete Payment</Button>
