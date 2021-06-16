93b72f86342d669abe57271da3f46216325e14a6
Hampus Olausson <hampus.john.olausson@gmail.com>
2021-06-16 12:21:38 +0200

    image router done

---
+++ Server/Resources/Image/image.router.ts
@@ -0,0 +1,32 @@
+import express from 'express'
+const controller = require('./image.controller')
+const ImageRouter = express.Router()
+import multer from 'multer';
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, './uploads')
+    }, filename: function(req, file, cb){
+        cb(null, Math.floor(Math.random() * 2000) + file.originalname)
+    }
+})
+
+const upload = multer({
+    storage: storage, 
+    limits: {
+        fileSize: 1024 * 1024 * 5
+    },
+    fileFilter: (req, file, cb) => {
+        if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+            cb(null, true);
+        } else {
+            cb(null, false)
+        }
+    }
+});
+
+ImageRouter
+.post('uploadImage', upload.single('image'), controller.uploadImage)
+.get('./getImage/:id', controller.getImage)
+
+export default ImageRouter
